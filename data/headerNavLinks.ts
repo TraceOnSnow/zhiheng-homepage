5d37b8d931a14772b75cfdb58dfa075878a3fc24
TraceOnSnow <wzh020212@outlook.com>
2026-02-24 17:57:34 -0600

    update homepage & about content

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -4,6 +4,7 @@ const headerNavLinks = [
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
+  { href: '/static/files/Resume_zhiheng.pdf', title: 'Resume' },
 ]
 
 export default headerNavLinks
