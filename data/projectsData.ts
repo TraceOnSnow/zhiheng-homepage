3582dc15d1840263347254daee42bd7c59d8ff88
TraceOnSnow <wzh020212@outlook.com>
2026-02-27 23:48:40 -0600

    upd project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,12 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'SteamGuess',
+    description: `SteamGuess is a fun and interactive game where players guess Steam games based on
+    clues like popularity, reviews, release dates, and more. With a sleek interface and
+    dynamic feedback, it’s perfect for gamers who love a challenge.`,
+    imgSrc: '/static/images/steamguess.png',
+    href: 'https://github.com/your-repo/SteamGuess',
   },
 ]
 
